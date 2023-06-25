02125174c48e31719b8a9c0e4a56d84a9c88a853
lfdavilago <30605061+lfdavilago@users.noreply.github.com>
2023-06-25 14:36:25 -0500

    Update MyHeader.js

--- components/MyHeader.js
+++ components/MyHeader.js
@@ -5,13 +5,13 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div>
-            <nav class="desk"> 
+            <nav class="desk">
                 <ul>
                     <li><a href="index.html"><img src="img/LUDIN.png" alt="Esdek Logo" class="Esdeklogo"></a></li>
                     <li><a href="PresentacionProyecto.html"><button>Presentación del Proyecto</button></a></li>
                     <li><a href="Materiales.html"><button>Materiales</button></a></li>
                     <li><a href="Procedimiento.html"><button>Procedimiento</button></a></li>
-                    <li><a href="Resultados.html"><button>Resultados</button></a></li> 
+                    <li><a href="Resultados.html"><button>Resultados</button></a></li>
                 </ul>
             </nav>
         </div>
